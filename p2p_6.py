5238992404f381f883c13e5fb2e52cab5b95019b
Sushant16sk <82380408+Sushant16sk@users.noreply.github.com>
2021-05-29 14:35:21 +0530

    Add files via upload

---
+++ p2p_6.py
@@ -0,0 +1,13 @@
+#leap year
+
+n=int(input('enter year: '))
+if n%4==0:
+    if n%100==0:
+        if n%400==0:
+            print('leap year')
+        else:
+            print('not a leap year')
+    else:
+         print('leap year')
+else:
+    print('not a leap year')
